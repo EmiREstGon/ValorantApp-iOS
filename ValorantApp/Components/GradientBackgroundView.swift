699d00f53cd2acaae4765224b25e20123902e8dd
EmiREstGon <alu0101389240@ull.edu.es>
2024-07-01 13:45:08 +0100

    Few design adjustements & added gradient background

---
+++ ValorantApp/Components/GradientBackgroundView.swift
@@ -0,0 +1,8 @@
+//
+//  GradientBackgroundView.swift
+//  ValorantApp
+//
+//  Created by Emilio Rafael Estévez González on 1/7/24.
+//
+
+import Foundation
---
+++ ValorantApp/Components/UIViewControllerRepresentable.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewControllerRepresentable.swift
+//  ValorantApp
+//
+//  Created by Emilio Rafael Estévez González on 27/6/24.
+//
+
+import Foundation
